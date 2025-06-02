66aa8bce96eedd4ad5d497e3efadaecb6fc081e0
RexRichmond <rex.richmond@vectorremote.com>
2025-06-02 12:22:18 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+// src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const App = () => <h1>Hello from the ROI Calculator!</h1>;
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
